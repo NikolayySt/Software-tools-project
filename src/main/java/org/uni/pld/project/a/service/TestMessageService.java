3b7f2c7f5cf9b449516e30f725402807cb9f2ae5
nstoilov <stoilov@media-it-services.de>
2021-05-15 17:33:27 +0300

    Make a change to test Travis ci

--- src/main/java/org/uni/pld/project/a/service/TestMessageService.java
+++ src/main/java/org/uni/pld/project/a/service/TestMessageService.java
@@ -1,4 +1,4 @@
-package org.uni.pld.project.a;
+package org.uni.pld.project.a.service;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
