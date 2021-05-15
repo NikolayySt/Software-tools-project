0951ff6ca269bcbb566cc614f511ce60a620a117
nstoilov <stoilov@media-it-services.de>
2021-05-15 11:34:58 +0300

    Initial push for project

---
+++ Project-A/src/main/java/org/uni/pld/Project/A/ProjectAApplication.java
@@ -0,0 +1,13 @@
+package org.uni.pld.Project.A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAApplication.class, args);
+	}
+
+}
---
+++ Project-A/src/main/java/org/uni/pld/Project/A/config/Configuration.java
@@ -0,0 +1,9 @@
+package org.uni.pld.Project.A.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@org.springframework.context.annotation.Configuration
+@EnableWebMvc
+public class Configuration {
+
+}
