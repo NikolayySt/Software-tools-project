0c07a8b1a48d863fbff495094c808c73d96e6ad2
NikolayySt <nikolayy1995@gmail.com>
2021-05-15 12:09:31 +0300

    Delete Project-A directory

--- Project-A/src/main/java/org/uni/pld/Project/A/config/Configuration.java
+++
@@ -1,9 +0,0 @@
-package org.uni.pld.Project.A.config;
-
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-@org.springframework.context.annotation.Configuration
-@EnableWebMvc
-public class Configuration {
-
-}
