47597e642ae8301a9e984ef637c00af9de2d3d2c
nstoilov <stoilov@media-it-services.de>
2021-05-16 10:41:39 +0300

    test

--- src/main/java/org/uni/pld/project/a/service/TestMessageService.java
+++ src/main/java/org/uni/pld/project/a/service/TestMessageService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestMessageService {
 
     public TestMessageService() {
-        System.out.println("Service initialized");
+        System.out.println("Service initialized!");
     }
 
     @GetMapping
